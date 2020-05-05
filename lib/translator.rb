c3980f1615fa23256ec4039fecc5e75e9488febf
Ziming Mao <ziming.mao@yale.edu>
2020-05-05 00:49:53 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,47 @@
 # require modules here
-
-def load_library
+require "yaml"
+def load_library(address)
   # code goes here
+  h = YAML.load_file(address)
+  output = {}
+  h.reduce({}) do |memo, (key, value)|
+    p = {
+      :english => value[0],
+      :japanese => value[1]
+    }
+    output[key] = p
+    memo
+  end
+  output
+
 end
 
-def get_japanese_emoticon
+def get_english_meaning(address, emoji)
   # code goes here
+  h = load_library(address)
+
+  h.reduce({}) do |memo, (key, value)|
+    if value[:japanese] == emoji
+
+      return key.to_s
+    end
+  end
+  return "Sorry, that emoticon was not found"
+
 end
 
-def get_english_meaning
+
+def get_japanese_emoticon (address, emoji)
   # code goes here
+  h = load_library(address)
+
+  h.reduce({}) do |memo, (key, value)|
+    if value[:english] == emoji
+
+      return value[:japanese]
+    end
+  end
+
+  return "Sorry, that emoticon was not found"
+
 end
