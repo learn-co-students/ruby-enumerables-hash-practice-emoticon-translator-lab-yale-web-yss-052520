9b05ab6164b38c5976cd7f784bd529cb0ab31ed0
Justin James <justin.james@yale.edu>
2020-05-10 00:11:02 +0000

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require_relative "../lib/translator.rb"
 
 RSpec.configure do |config|
-  # config here
+#  # config here
+ # config = YAML::load_file(seed_file)
+#  seed_file = Rails.root.join('lib', 'translator', 'emoticons.yml')
+#config = YAML::load_file(seed_file)
+#Category.create!(config)
 end
