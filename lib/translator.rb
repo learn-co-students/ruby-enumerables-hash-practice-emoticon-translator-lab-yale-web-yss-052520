f2e5dd2cff2d86d33acc19606e4aa5441e418991
Enrique Valencia <enrique.valencia@yale.edu>
2020-05-22 18:14:40 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,20 +1,25 @@
 # require modules here
+#require 'pry'
 def load_library(file_name)
   require 'yaml'
   final_hash= {}
-  faces_hash= {}
   old_hash = YAML.load_file(file_name)
-  old_hash.each do |names, faces|
-    old_hash[name] do |face|
+  old_hash.each do |name, faces|
+    #binding.pry
+    faces_hash = {}
+    old_hash[name].each do |face|
       if face == old_hash[name][0]
         final_hash[name] = faces_hash
         faces_hash[:english] = face
+        #binding.pry
       elsif face == old_hash[name][1]
         faces_hash[:japanese] = face
+        #binding.pry
       end
     end
   end
   final_hash
+  #binding.pry
 end
 
 def get_japanese_emoticon
